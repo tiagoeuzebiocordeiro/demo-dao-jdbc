cc2597f3426f1094f86679f646b427dbe22008a3
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-06-26 21:23:47 -0300

    SellerDaoJDBC(impl) + DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.util.Date;
@@ -11,5 +13,11 @@ public class Program {
         System.out.println(obj);
         Seller obj2 = new Seller(1, "Tiago", "tiago@mail.com",new Date(), 12000.00, obj);
         System.out.println(obj2);
+
+        /*My program doesn't know the impl, only knows the interface, this is a
+        method to make a
+        dependecy injection
+         hiding the impl.*/
+        SellerDao dao = DaoFactory.createSellerDao();
     }
 }
