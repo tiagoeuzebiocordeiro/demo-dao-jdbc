38067caa92f49574c9cac6850995a8c368903444
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-06-26 21:00:43 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+        Seller obj2 = new Seller(1, "Tiago", "tiago@mail.com",new Date(), 12000.00, obj);
+        System.out.println(obj2);
     }
 }
